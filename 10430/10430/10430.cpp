902178c0f145369903f4e29a52302bd977f7cd5c
Park <pjw9195@naver.com>
2019-03-14 02:04:17 +0900

    나머지 연산

---
+++ 10430/10430/10430.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+	int A, B, C;
+	cin >> A >> B >> C;
+	cout << (A + B) % C << endl << (A%C + B%C) % C << endl << (A*B) % C << endl << (A%C * B%C) % C;
+	return 0;
+}
+
