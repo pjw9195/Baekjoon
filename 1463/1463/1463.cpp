b2f9875c4dd3183929103822505b77bb7fa3d977
Park <pjw9195@naver.com>
2019-03-24 20:31:07 +0900

    1로 만들기(DP)

--- 1463/1463/1463.cpp
+++ 1463/1463/1463.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 
 using namespace std;
-int res2;
-int d[10000] = {0};
+
+int d[1000001] = {0};
 int one(int n) {
 	if (n == 1) {
 		return 0;
@@ -10,16 +10,22 @@ int one(int n) {
 	if (d[n] > 0)return d[n];
 	d[n] = one(n - 1) + 1;
 	if (n % 2 == 0) {
-		int temp = one(n / 2) + 1;
-		if (d[n] > temp) d[n] = temp;
+		int temp = one(n/2) + 1;
+		if (d[n] > temp) {
+			d[n] = temp;
+		}
 	}
 	if (n % 3 == 0) {
-		int temp = one(n / 3) + 1;
-		if (d[n] > temp) d[n] = temp;
+		int temp = one(n/3) + 1;
+		if (d[n] > temp) { 
+			d[n] = temp;
+		}
 	}
+	return d[n];
 }
 int main() {
 	int N;
 	cin >> N;
 	cout << one(N);
+	return 0;
 }
