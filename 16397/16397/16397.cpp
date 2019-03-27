fe180391e3c98688f6d51d313c82fd6461b270c8
Park <pjw9195@naver.com>
2019-03-27 16:57:55 +0900

    탈출(BFS이용)

---
+++ 16397/16397/16397.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<queue>
+#include<math.h>
+using namespace std;
+queue <pair <int, int>> que;
+int visit[100000];
+int main() {
+	int N, G, T;
+	int x, y ,x2;
+	cin >> N >> G >> T;
+	que.push(pair<int, int>(N, 0));
+	if (N == T) {
+		cout << "0";
+		return 0;
+	}
+	if (que.front().first >= 50000 && T < N) { 
+		cout << "ANG"; 
+		return 0;
+	}
+	while (!que.empty()) {
+		x = que.front().first;
+		y = que.front().second;
+		if (x == T) {
+			cout << y;
+			return 0;
+		}
+		for (int i = 5; i >= 0; i--) {
+			if (x * 2 >= pow(10, i)) {
+				x2 = x * 2 - pow(10, i);
+				break;
+			}
+		}
+		if (visit[x + 1] == 0 && x+1 <= 99999) {
+			que.push(pair<int, int>(x + 1, y + 1));
+			visit[x + 1] = 1;
+		}
+		if (visit[x2] ==0 && x != 0 && 2 * x <= 99999) {
+			que.push(pair<int, int>(x2, y + 1));
+			visit[x2] = 1;
+		}
+		que.pop();
+		if (que.front().second > G) {
+			cout << "ANG";
+			return 0;
+		}
+	}
+	cout << "ANG";
+}
