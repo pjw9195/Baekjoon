a7cb68c2c6c1e1f65a13ce59e10082099a7fe3f7
Park <pjw9195@naver.com>
2019-03-31 01:04:29 +0900

    1학년(DP 이용)

--- 5557/5557/5557.cpp
+++ 5557/5557/5557.cpp
@@ -1,32 +1,31 @@
 #include<iostream>
 #include<queue>
 using namespace std;
-int arr[501];
+int arr[102];
+long long D[102][21];
 int res = 0;
-int N,K;
-void DFS(int a,int b) {
-	if (b == N - 1) {
-		if (a == K) {
-			res++;
-		}
-		return;
-	}
-	if (a + arr[b] >= 0 && a + arr[b] <= 20) {
-		DFS(a + arr[b], b + 1);
-	}
-	if (a - arr[b] >= 0 && a - arr[b] <= 20) {
-		DFS(a - arr[b], b + 1);
-	}
-}
+int N;
 
 int main() {
-	int x, y;
 	int sum = 0;
 	cin >> N;
 	for (int i = 0; i < N; i++) {
 		cin >> arr[i];
 	}
-	K = arr[N - 1];
-	DFS(arr[0], 1);
-	cout << res;
+	D[0][arr[0]] = 1;
+	for (int i = 1; i <= N-2; i++) {
+		for (int j = 0; j <= 20; j++) {
+			if (D[i - 1][j] > 0) {
+				if (j - arr[i] >= 0) {
+					D[i][j - arr[i]] = D[i][j - arr[i]] + D[i - 1][j];
+				}
+				if (j + arr[i] <= 20) {
+					D[i][j + arr[i]] = D[i][j + arr[i]] + D[i - 1][j];
+				}
+			}
+		}
+	}
+	long long ans = D[N - 2][arr[N - 1]];
+	cout << ans<<'\n';
+	return 0;
 }
