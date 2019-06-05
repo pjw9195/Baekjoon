fd5b8e4fc7807d6599c9af7f6a4e21dabd4f40ff
Park <pjw9195@naver.com>
2019-06-05 17:18:03 +0900

    팬덤이 넘쳐흘러(짧게수정)

--- 17262/17262/17262.cpp
+++ 17262/17262/17262.cpp
@@ -1,31 +1,22 @@
 #include<iostream>
-#include<deque>
 #include<algorithm>
 using namespace std;
-deque <pair<int, int>> deq;
-deque <pair<int, int>> deq2;
 int main() {
 	int N;
-	int temp;
 	int x, y;
-	int first = 0;
-	int second = 0;
+	int maxx = 0;
+	int	miny = 999999;
 	cin >> N;
 	for (int i = 0; i < N; i++) {
 		cin >> x >> y;
-		deq.push_back(pair<int, int>(y, x));
-		deq2.push_back(pair<int, int>(x, y));
+		maxx = max(maxx, x);
+		miny = min(miny, y);
 	}
-	sort(deq.begin(), deq.end());
-	sort(deq2.begin(), deq2.end());
-	
-	temp = deq.size();
-	if (deq2[temp - 1].first > deq.front().first) {
-
-		cout << deq2[temp - 1].first - deq.front().first;
+	if (miny >= maxx) {
+		cout << 0;
 	}
 	else {
-		cout << 0;
+		cout << maxx - miny;
 	}
 
 }
